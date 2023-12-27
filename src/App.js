bf14da9c9129858b00389058ce790a141622df33
Andoni1Barba <Andoni1barba@gmail.com>
2023-12-27 16:12:32 -0400

    guardar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import {Component} from 'react'
 
-class App extends Component {
+class Button extends Component {
+    
+    state = {}
+    constructor(props) {
+        super(props)
+        console.log('Ejecutando constructor', props);
+    }
+    componentDidMount() {
+        console.log('Ejecutando componentDidMount');
+    }
+    componentDidUpdate(prevProps,prevState) {
+        console.log('Ejecutando componentDidUpdate', prevProps,prevState);
+    }
+
+    componentWillUnmount() {
+        console.log('Ejecutando componentWillUnmount',this.props, this.state);
+    }
+
     render() {
+        console.log('Ejecutando render');
+        return (
+            <button onClick={() => this.setState({prop:1})}>Enviar</button>
+        )
+    }
+}
+
+class App extends Component {
+    state = {
+        valor: 3
+    }
+    render() {
+        console.log(this.state);
         return (
             <div>
                 <h1>React App</h1>
+                {this.state.valor === 3 ? <Button chanchito='feliz'/>: null}
+                <button className={`${this.state.valor}`} onClick={() => this.setState({valor: 2})}>Enviar En App</button>
             </div>
         )
     }
