2c40d0880480de56bda6590fdf196fcd7c1f1af9
Andoni1Barba <Andoni1barba@gmail.com>
2024-01-08 13:34:50 -0400

    si

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
 import {Component} from 'react'
 
-class Button extends Component {
-    
-    state = {}
-    constructor(props) {
-        super(props)
-        console.log('Ejecutando constructor', props);
-    }
-    componentDidMount() {
-        console.log('Ejecutando componentDidMount');
-    }
-    componentDidUpdate(prevProps,prevState) {
-        console.log('Ejecutando componentDidUpdate', prevProps,prevState);
-    }
-
-    componentWillUnmount() {
-        console.log('Ejecutando componentWillUnmount',this.props, this.state);
-    }
-
+class Input extends Component {
     render() {
-        console.log('Ejecutando render');
         return (
-            <button onClick={() => this.setState({prop:1})}>Enviar</button>
+            <input
+                value={this.props.value}
+                onChange={this.props.onChange}
+            />
         )
     }
 }
 
 class App extends Component {
     state = {
-        valor: 3
+        nombre: '',
+        apellido: ''
+    }
+    updateNombre(v){
+        this.updateValues('nombre',v.target.value)
+    }
+    updateValues(prop,value){
+        this.setState({[prop]:value})
     }
     render() {
-        console.log(this.state);
         return (
-            <div>
-                <h1>React App</h1>
-                {this.state.valor === 3 ? <Button chanchito='feliz'/>: null}
-                <button className={`${this.state.valor}`} onClick={() => this.setState({valor: 2})}>Enviar En App</button>
-            </div>
+            <p>
+                Nombre Completo: {`${this.state.nombre} ${this.state.apellido}`}
+                <Input 
+                    value={this.state.nombre}
+                    onChange={this.updateNombre}
+                />
+                <Input
+                    value={this.state.apellido}
+                    onChange={(e) => this.updateValues('apellido',e.target.value)}
+                />
+            </p>
         )
     }
 }
